df829753ce1116afa0940997b14d587bc6da4d75
Julio Sansossio <juliosansossio@gmail.com>
2024-05-05 17:36:58 -0400

    testing link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { YourCityComponent } from "../components/city/city.component";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -22,8 +23,8 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/testing"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -35,7 +36,7 @@ export default function Home() {
             </span>
           </h2>
           <YourCityComponent />
-        </a>
+        </Link>
       </div>
     </main>
   );
